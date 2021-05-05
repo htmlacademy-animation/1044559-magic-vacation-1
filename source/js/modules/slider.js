1418beed1cc24922f79192b65bfdd38b378b331e
Andrey Sychev <nukemoore@gmail.com>
2021-05-05 10:46:09 +0300

    add theme changing on history screen

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -5,6 +5,15 @@ export default () => {
   let sliderContainer = document.getElementById(`story`);
   sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
 
+  const changeHeaderClass = (classNumber) => {
+    const pageHeader = document.querySelector(`.page-header`);
+    const headerClasses = [`page-header--water`, `page-header--ice`, `page-header--dark`];
+    pageHeader.classList.remove(...headerClasses);
+    if (typeof classNumber === `number`) {
+      pageHeader.classList.add(headerClasses[classNumber]);
+    }
+  };
+
   const setSlider = function () {
     if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
       storySlider = new Swiper(`.js-slider`, {
@@ -18,12 +27,16 @@ export default () => {
         on: {
           slideChange: () => {
             if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
+              changeHeaderClass();
               sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
             } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
+              changeHeaderClass(0);
               sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
             } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
+              changeHeaderClass(1);
               sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
             } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
+              changeHeaderClass(2);
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
             }
           },
@@ -52,12 +65,16 @@ export default () => {
         on: {
           slideChange: () => {
             if (storySlider.activeIndex === 0) {
+              changeHeaderClass();
               sliderContainer.style.backgroundImage = `url("img/slide1.jpg")`;
             } else if (storySlider.activeIndex === 2) {
+              changeHeaderClass(0);
               sliderContainer.style.backgroundImage = `url("img/slide2.jpg")`;
             } else if (storySlider.activeIndex === 4) {
+              changeHeaderClass(1);
               sliderContainer.style.backgroundImage = `url("img/slide3.jpg")`;
             } else if (storySlider.activeIndex === 6) {
+              changeHeaderClass(2);
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
             }
           },
